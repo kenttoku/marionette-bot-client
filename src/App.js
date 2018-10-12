0fe518b8c0cb41b8278929144bf97cc740c22605
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-12 09:17:57 -0400

    Move client out of server repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { authenticate } from './actions/actions';
+import { Route } from 'react-router-dom';
+import Header from './components/header';
+
+export class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(authenticate())
+  }
+
+  render() {
+    return (
+      <Route path='/' component={Header} />
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  authToken: state.authToken
+});
+
+export default connect(mapStateToProps)(App);
