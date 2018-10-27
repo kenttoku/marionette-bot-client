4531ad37d1dc03e78ee54ff711d788ae43c5dd82
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-27 01:15:46 -0400

    Remove all components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <a href="/auth/google">Sign In With Discord</a>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/login-button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LoginButton() {
-  return (
-    <a href="http://localhost:8080/auth/discord">Log in with discord </a>
-  );
-}
-
-export default LoginButton;
